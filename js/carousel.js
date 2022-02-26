4596d39582b6c75900a1f1c3df81d324e73a4b14
Adel <gerencser.adel@gmail.com>
2022-02-26 18:06:06 +0100

    hamburger menu style

--- js/carousel.js
+++ js/carousel.js
@@ -14,36 +14,38 @@ function moveCarousel() {
 
 setInterval(moveCarousel, 6000);
 
-/* OPEN NAVIGATION */
+
+/* VARIABLES */
 
 let openNav = document.querySelector(".js-open-navigation");
 let navContainer = document.querySelector(".js-header-nav-container");
-let headerMenu = document.querySelector(".js-header-menu");
-let headerMenuLi = document.querySelector(".js-menu__li");
-let headerMenuLiLink = document.querySelector(".js-header-li__link");
+let headerMenuUl = document.querySelector(".js-header-menu");
+let headerMenuLi = document.querySelector(".js-header-menu__li");
 let headerButtons = document.querySelector(".js-header-buttons");
 
 let menuExit = document.querySelector(".js-menu-exit");
 let menuOpen = document.querySelector(".js-menu-open");
+
+
+/* OPEN NAVIGATION */
 
 menuOpen.addEventListener("click", () => {
     menuOpen.classList.add("js-d-none");
     menuExit.classList.add("js-exit");
     openNav.classList.add("js-open-nav");
     navContainer.classList.add("js-nav-container");
-    headerMenu.classList.add("js-menu");
-    headerMenuLi.classList.add("js-menu__li");
-    headerMenuLiLink.classList.add("js-m-li__link");
+    headerMenuUl.classList.add("js-menu");
     headerButtons.classList.add("js-h-buttons");
 });
+
+
+/* CLOSE NAVIGATION */
 
 menuExit.addEventListener("click", () => {
     menuOpen.classList.remove("js-d-none");
     menuExit.classList.remove("js-exit");
     openNav.classList.remove("js-open-nav");
     navContainer.classList.remove("js-nav-container");
-    headerMenu.classList.remove("js-menu");
-    headerMenuLi.classList.remove("js-menu__li");
-    headerMenuLiLink.classList.remove("js-m-li__link");
+    headerMenuUl.classList.remove("js-menu");
     headerButtons.classList.remove("js-h-buttons");
 });
